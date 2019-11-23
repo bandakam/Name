89a24132d0ff72e34a9e4db95bd06ffbbae81420
banda <banda@wszib.edu.pl>
2019-11-23 15:57:20 +0100

    commit11

--- src/Name.java
+++ src/Name.java
@@ -1,6 +1,8 @@
 public class Name {
     public static void main(String[] args) {
         System.out.println("Kamil");
+        System.out.println("Kamil");
+        System.out.println("Kamil");
 
     }
 }
